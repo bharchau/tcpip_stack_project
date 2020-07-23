68c5a29aff797fad7cc5a72a5dff8b6c17f2cfdf
bharchau <2692bharat@gmail.com>
2020-07-24 03:14:25 +0530

    Graph topology implementation

---
+++ graph.c
@@ -0,0 +1,98 @@
+#include "graph.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <memory.h>
+
+
+void 
+insert_link_between_two_nodes(node_t *node1, node_t* node2, 
+                            char *from_if_name,char *to_if_name,
+                            unsigned int cost)
+{
+    link_t *link = calloc(1, sizeof(link_t));
+    int empty_intf_slot;
+
+    strncpy(link->intf1.if_name, from_if_name, IF_NAME_SIZE);
+    link->intf1.if_name[IF_NAME_SIZE - 1] = '\0';
+    strncpy(link->intf2.if_name, to_if_name, IF_NAME_SIZE); 
+    link->intf2.if_name[IF_NAME_SIZE - 1] = '\0';
+
+    link->intf1.link = link;
+    link->intf2.link = link;
+
+    link->intf1.att_node = node1;
+    link->intf2.att_node = node2;
+    link->cost = cost;
+
+    /* 
+     * interface link properties is created.
+     * Now insert these links to the empty interface node.
+     */
+    empty_intf_slot = get_node_intf_available_slot(node1);
+    node1->intf[empty_intf_slot] = &link->intf1;
+    
+    empty_intf_slot = get_node_intf_available_slot(node2);
+    node2->intf[empty_intf_slot] = &link->intf2;
+}
+graph_t*
+create_new_graph(char *topology_name)
+{
+    graph_t *graph = calloc(1,sizeof(graph_t));
+    strncpy(graph->topology_name, topology_name, 32);
+    graph->topology_name[31] = '\0';
+    
+    init_glthread(&graph->node_list);
+    return graph;
+}
+
+node_t*
+create_graph_node(graph_t *graph, char *node_name)
+{
+    node_t *node = calloc(1, sizeof(node_t));
+    strncpy(node->node_name, node_name,NODE_NAME_SIZE);
+    node->node_name[NODE_NAME_SIZE - 1] = '\0';
+
+    init_glthread(&node->graph_glue);
+    glthread_add_next(&graph->node_list, &node->graph_glue);
+    return node;
+}
+
+void dump_graph(graph_t *graph)
+{
+    node_t *node;
+    glthread_t *curr;
+
+    printf("%s:\n", graph->topology_name);
+    printf("Node Name\t\tInterface Name\t\tNbr Node\t\tLocal Node\t\tCost\n");
+    printf("====================================================================================================\n");
+    ITERATE_GLTHREAD_BEGIN(&graph->node_list, curr) {
+        node = graph_glue_to_node(curr);
+        dump_node(node);
+    } ITERATE_GLTHREAD_END(&graph->node_list, curr);
+}
+
+void dump_node(node_t *node) 
+{
+    unsigned int i = 0;
+    interface_t *intf;
+
+    printf("\r%s\t\t\t", node->node_name);
+
+    for (i = 0; i < MAX_INTF_PER_NODE; i++) {
+        intf = node->intf[i];
+        if(!intf) break; // Empty interface slot
+        dump_interface(intf);
+    }
+    printf("\n");
+}
+
+void dump_interface(interface_t *interface)
+{
+    link_t *link = interface->link;
+    node_t *nbr_node = get_nbr_node(interface);
+
+    printf("%s\t\t\t%s\t\t\t%s\t\t\t%u\n\t\t\t",
+            interface->if_name, nbr_node->node_name, 
+            interface->att_node->node_name, link->cost);
+}
---
+++ graph.h
@@ -0,0 +1,132 @@
+
+#ifndef __GRAPH__
+#define __GRAPH__
+
+#include <assert.h>
+#include "gluethread/glthread.h"
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct node_ node_t;
+typedef struct link_ link_t;
+
+#define NODE_NAME_SIZE   16
+#define IF_NAME_SIZE     16
+#define MAX_INTF_PER_NODE   10
+
+
+typedef struct interface_ {
+    char if_name[IF_NAME_SIZE];
+    struct link_ *link;
+    struct node_ *att_node;
+} interface_t;
+
+struct link_ {
+    interface_t intf1; // Host side connection of node
+    interface_t intf2; // Peer side connection of nbr node
+    unsigned int cost;
+};
+
+struct node_ {
+    char node_name[NODE_NAME_SIZE];
+    interface_t *intf[MAX_INTF_PER_NODE];
+    glthread_t graph_glue;
+};
+
+typedef struct graph_ {
+    char topology_name[32];
+    glthread_t node_list;
+}graph_t;
+
+GLTHREAD_TO_STRUCT(graph_glue_to_node, node_t, graph_glue);
+/* 
+ *Check who is my neighbour node/device connected to interface_t *interface
+ */
+static inline node_t*
+get_nbr_node(interface_t *interface)
+{
+    link_t *link;
+    assert(interface->att_node);
+    assert(interface->link);
+
+    /* Get interface link
+     * Figure out which interface this link belongs to (host or peer)
+     * i.e is this link belonging to intf1 or intf2.
+     */
+    link = interface->link;
+    if (&link->intf1 == interface) {
+        /* My nbr is intf2 */
+        return (link->intf2.att_node);
+    } else {
+        /* My nbr is intf1 */
+        return (link->intf1.att_node);
+        
+    }
+}
+
+/* 
+ * Check which interface are not connected to node/device 
+ */
+static inline int 
+get_node_intf_available_slot(node_t *node)
+{
+    int intf_idx;
+
+    /* 
+     * Loop the node interface to MAX and check if any node intf pointer is NULL
+     * If NULL, it means node interface is available for connection.
+     */
+    for (intf_idx = 0; intf_idx < MAX_INTF_PER_NODE; intf_idx++) {
+        if(node->intf[intf_idx]) {
+            continue;
+        }
+        return intf_idx;
+    }
+
+    /* No empty interface in node is avoilable. */
+    return -1;
+}
+
+static inline interface_t*
+get_node_if_by_name(node_t *node, char *if_name)
+{
+    int i;
+    for (i = 0; i < IF_NAME_SIZE; i++) {
+        if(strncmp(node->intf[i]->if_name, if_name, IF_NAME_SIZE) == 0) {
+            return node->intf[i];
+        }
+    }
+    return NULL;
+}
+
+static inline node_t*
+get_node_by_node_name(graph_t *topo, char *node_name)
+{
+    node_t *node;
+    glthread_t *curr;
+
+    ITERATE_GLTHREAD_BEGIN(&topo->node_list, curr){
+        node = graph_glue_to_node(curr);
+        if(strncmp(node->node_name, node_name, strlen(node_name)) == 0)
+            return node;
+    } ITERATE_GLTHREAD_END(&topo->node_list, curr);
+    return NULL;
+}
+node_t *
+create_graph_node(graph_t* graph, char* node_name);
+
+graph_t *
+create_new_graph(char* topology_name);
+
+void 
+insert_link_between_two_nodes(node_t * node1, node_t* node2,
+                              char *from_if_name,
+                              char *to_if_name,
+                              unsigned int cost);
+
+/*Display Routines*/
+void dump_graph(graph_t *graph);
+void dump_node(node_t *node);
+void dump_interface(interface_t *interface);
+
+#endif 
